5458040357f8280806a46b61d9c8f21922e6141f
Dipendrachy <chysunnygamer9@gmail.com>
2021-06-29 10:32:14 +0545

    new pages added

---
+++ javascript/javascript.js
@@ -0,0 +1,8 @@
+
+function myFucntion(){
+    console.log("Button clicked!");
+    window.alert("Button click alert!");
+    document.write("This is content from javascript");
+}
+
+document.getElementById("para1").innerHTML="paragraph from javascript";
